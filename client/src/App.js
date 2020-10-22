b62f82c18e8ae1c0c5f283674098e8f750792a95
hansupie <hannemarikuusisto@gmail.com>
2020-10-22 19:30:28 +0300

    basis for ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,36 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
-import Axios from "axios";
+import LoginForm from './loginForm';
+import {
+  BrowserRouter as Router,
+  Switch, Route, Link
+} from "react-router-dom"
+import Home from './home';
+import AddScenarioForm from "./addScenario";
 
 function App() {
-const [scenarioNameState , setScenarioName] = useState("");
-const [scenarioTypeState , setScenarioType] = useState("");
-
-const submitScenario = () => {
-
-  Axios.post('http://localhost:3001/api/insert',{
-    scenarioNameVar: scenarioNameState,
-    scenarioTypeVar: scenarioTypeState,
-  }).then (() => {
-    alert("Scenario Added to DB");
-  });
-};
-
   return (
     <div className="App">
-      <h1> Hello PMG</h1>
-      <div className="form">
-        <label>Scenario Name</label>
-        <input
-         type="text" 
-         name="scenarioName"
-         onChange={(e) => {setScenarioName(e.target.value);}}
-         ></input>
-        <label>Scenario type</label>
-        <input 
-        type="text" 
-        name="scenarioType"
-        onChange={(e) => {setScenarioType(e.target.value);}}></input>
-        
-        
-        <button onClick={submitScenario}> Submit</button>
-
+      <div className="container">
+        <Router>
+          <div>
+            <Link className="nav" to="/">PMG</Link>
+            <Link className="nav" to="/forteachers">For Teachers</Link>
+            <Link className="nav" to="/addscenario">Add Scenario</Link>
+          </div>
+          <Switch>
+            <Route path="/forteachers">
+              <LoginForm/>
+            </Route>
+            <Route path="/addscenario">
+              <AddScenarioForm/>
+            </Route>
+            <Route path="/">
+              <Home/>
+            </Route>
+          </Switch>
+        </Router>
       </div>
-
     </div>
   );
 }
