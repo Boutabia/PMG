7259f27e7eb97e61ffd71bbcae24aead574350ea
Reetta Verkasalo <reetta.verkasalo@tuni.fi>
2021-02-05 21:43:15 +0200

    rich text rendering & DOMPurify package

--- client/src/Explanation.js
+++ client/src/Explanation.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
+import DOMPurify from 'dompurify';
+
 
 function Explanation({text}) {
     return (
         <Card className='explanation'>
             <Card.Title>Explanation</Card.Title>
-            <Card.Text>{text}</Card.Text>
+            <Card.Text dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(text)}}/>
         </Card>
     )
 }
